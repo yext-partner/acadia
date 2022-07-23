66640765984b17eb1e728df5d4238d305035f0d5
lymarrie <lucmarrie1@gmail.com>
2022-07-23 17:51:39 -0400

    search card css

--- src/components/search/cards/GenericCard.tsx
+++ src/components/search/cards/GenericCard.tsx
@@ -11,21 +11,23 @@ import {
       result.description) as string;
   
     return (
-      <div className="border rounded-md p-4">
-        {title && <h3 className="">{renderHighlightedValue(title)}</h3>}
-        {description && (
-          <p className="text-sm text-gray-500 max-h-24 overflow-hidden">
-            {renderHighlightedValue(description)}
-          </p>
-        )}
-        {/* <pre>{JSON.stringify(result.rawData, null, 2)}</pre> */}
-        <a
-          href={result.rawData.slug as string}
-          className=" mt-2 flex items-center gap-1 "
-        >
-          View Details
-          <FaChevronRight />
-        </a>
+      <div className="border rounded-md p-4 search-card bg-white">
+        <div>
+          {title && <h3 className="">{renderHighlightedValue(title)}</h3>}
+          {description && (
+            <p className="text-sm text-gray-500 max-h-24 overflow-hidden">
+              {renderHighlightedValue(description)}
+            </p>
+          )}
+          {/* <pre>{JSON.stringify(result.rawData, null, 2)}</pre> */}
+          <a
+            href={result.rawData.slug as string}
+            className=" mt-2 flex items-center gap-1 "
+          >
+            View Details
+            <FaChevronRight />
+          </a>
+        </div>
       </div>
     );
   };
