8c2b6d0d6980138431628d1e644e2bf367118bdd
lymarrie <lucmarrie1@gmail.com>
2023-07-27 16:09:17 -0400

    image component header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import Cta from "../components/cta";
+import { Image } from "@yext/pages/components";
+
 
 export interface HeaderProps {
   _site?: any;
@@ -10,7 +12,7 @@ const Header = (props: HeaderProps) => {
   const { _site } = props;
   const name = _site.name 
   const address = _site.c_relatedFacility[0].address;
-  const logo = _site.logo.image.url;
+  const logo = _site.logo.image;
   const primaryColor = _site.c_primaryColor;
   const secondaryColor = _site.c_secondaryColor;
 
@@ -21,7 +23,7 @@ const Header = (props: HeaderProps) => {
         <div className="centered-container">
           <nav className="py-6 flex items-center justify-between text-white">
             <div className="flex items-center space-x-5">
-              {logo && <a className="hover:no-underline" href="/"><img src={logo} width="120" height="120"/></a>}
+              {logo && <a className="hover:no-underline" href="/"><Image image={logo} layout="fixed" width={120} height={120}></Image></a>}
               <a className="link hidden md:block hover:no-underline" href="/about-your-dentist">About Your Dentist</a>
               <a className="link hidden md:block hover:no-underline" href="/payment-options">Payment Options</a>
               <a className="link hidden md:block hover:no-underline" href="/new-patients">New Patients</a>
